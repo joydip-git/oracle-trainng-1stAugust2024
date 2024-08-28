e7b2239640f0db7c6437eee5a8bfa130a34ff1b1
Joydip <joydip.mondal@gmail.com>
2024-08-28 13:05:51 +0530

    hibernate with hibernate template

---
+++ codes/4-backend/day-2/hibernatewithspring-v2/firsthibernateapp/src/main/java/com/hibernateapps/repository/UserRepository.java
@@ -0,0 +1,105 @@
+package com.hibernateapps.repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+//import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.dao.DataAccessException;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+
+import com.hibernateapps.models.User;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+
+public class UserRepository implements RepositoryContract<User>, AutoCloseable {
+
+    // private SessionFactory factory;
+    private HibernateTemplate template;
+
+    // public UserRepository(SessionFactory factory) {
+    // this.factory = factory;
+    // }
+
+    public UserRepository(HibernateTemplate template) {
+        this.template = template;
+    }
+
+    @Override
+    public boolean add(User data) {
+        try {
+            Serializable savedData = template.save(data);
+            return savedData != null;
+        } catch (DataAccessException e) {
+            throw e;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    @Override
+    public boolean update(int id, User data) {
+        try {
+            User currentUser = template.get(User.class, id);
+            if (currentUser != null) {
+                currentUser.setUserName(data.getUserName());
+                template.update(currentUser);
+                return true;
+            } else
+                return false;
+        } catch (DataAccessException e) {
+            throw e;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    @Override
+    public boolean delete(int id) {
+        try {
+            User currentUser = template.get(User.class, id);
+            if (currentUser != null) {
+                template.delete(currentUser);
+                return true;
+            } else
+                return false;
+        } catch (DataAccessException e) {
+            throw e;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    @Override
+    public List<User> getAll() {
+        try {
+            return template.loadAll(User.class);
+        } catch (DataAccessException e) {
+            throw e;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    @Override
+    public User get(int id) {
+        try {
+            return template.get(User.class, id);
+        } catch (DataAccessException e) {
+            throw e;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (factory != null)
+            factory.close();
+    }
+
+}
