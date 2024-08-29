119a5672cd8dc9cf370be8b89a75ed5a7779b143
Joydip <joydip.mondal@gmail.com>
2024-08-29 15:04:09 +0530

    Update UserController.java

--- codes/4-backend/day-3/userservice/src/main/java/oracle/springboot/apps/userservice/controllers/UserController.java
+++ codes/4-backend/day-3/userservice/src/main/java/oracle/springboot/apps/userservice/controllers/UserController.java
@@ -7,7 +7,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,8 +30,29 @@ public class UserController {
     private UserRepository repository;
 
     @GetMapping(path = "users", produces = "application/json")
-    public List<User> getUsers() {
-        return repository.getAll();
+    public ResponseEntity<List<User>> getUsers() {
+        try {
+            List<User> all = repository.getAll();
+            // pass Optional<T> to of() method
+            return ResponseEntity.of(Optional.of(all));
+        } catch (Exception e) {
+            return ResponseEntity
+                    .of(ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage()))
+                    .build();
+        }
+    }
+
+    @GetMapping(path = "users/{id}", produces = "application/json")
+    public ResponseEntity<User> getUser(@PathVariable(name = "id") int id) {
+        try {
+            User found = repository.get(id);
+            if (found != null) {
+                return ResponseEntity.ok(found);
+            } else
+                return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
     @PostMapping(path = "users/add", produces = "application/json", consumes = "application/json")
@@ -43,6 +66,56 @@ public class UserController {
                         .path("/{id}")
                         .buildAndExpand(
                                 user.getUserId())
+                        .toUri();
+
+                return ResponseEntity
+                        .created(location)
+                        .build();
+            } else
+                return ResponseEntity
+                        .internalServerError()
+                        .build();
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(
+                            HttpStatus.INTERNAL_SERVER_ERROR)
+                    .build();
+        }
+    }
+
+    @DeleteMapping(path = "users/delete/{id}", produces = "application/json")
+    public ResponseEntity<Object> deleteUser(@PathVariable(name = "id") int id) {
+        try {
+            boolean status = repository.delete(id);
+            if (status) {
+                URI location = ServletUriComponentsBuilder
+                        .fromCurrentRequest()
+                        .buildAndExpand(id)
+                        .toUri();
+
+                return ResponseEntity
+                        .created(location)
+                        .build();
+            } else
+                return ResponseEntity
+                        .internalServerError()
+                        .build();
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(
+                            HttpStatus.INTERNAL_SERVER_ERROR)
+                    .build();
+        }
+    }
+
+    @PutMapping(path = "users/update/{id}", produces = "application/json", consumes = "application/json")
+    public ResponseEntity<Object> updateUser(@RequestBody User user, @PathVariable(name = "id") int id) {
+        try {
+            boolean status = repository.update(id, user);
+            if (status) {
+                URI location = ServletUriComponentsBuilder
+                        .fromCurrentRequest()
+                        .buildAndExpand()
                         .toUri();
 
                 return ResponseEntity
